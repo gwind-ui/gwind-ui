582981329d309c24e5ba3aaaff2139e350ed3d70
Lord Macbook's <lord@Lord-Macbooks.local>
2025-10-01 17:11:22 +0700

    b

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,34 +1,28 @@
 /** @type { import('@storybook/vue3-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/vue3-webpack5",
-    options: {},
-  },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(scss|sass)$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'sass-loader',
-          options: {
-            implementation: (await import('sass')).default,
-          },
-        },
-      ],
-    });
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: ["@storybook/addon-webpack5-compiler-swc", "@storybook/addon-onboarding", "@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions"],
+    framework: {
+        name: "@storybook/vue3-webpack5",
+        options: {},
+    },
+    webpackFinal: async config => {
+        config.module.rules.push({
+            test: /\.(scss|sass)$/,
+            use: [
+                "style-loader",
+                "css-loader",
+                {
+                    loader: "sass-loader",
+                    options: {
+                        implementation: (await import("sass")).default,
+                    },
+                },
+            ],
+        })
 
-    return config;
-  },
-};
+        return config
+    },
+}
 
-export default config;
+export default config
